917a8427448d0b9fae272671d647430f6a1adc82
Scarus <41557299+Eduard20CR@users.noreply.github.com>
2026-01-22 10:22:26 -0600

    Add ECS task definition and related variables for container configuration

--- terraform/modules/app/ecs/main.tf
+++ terraform/modules/app/ecs/main.tf
@@ -1,3 +1,5 @@
+# ECS Cluster
+
 resource "aws_ecs_cluster" "this" {
   name = "${var.project_identifier}-cluster"
 
@@ -21,5 +23,54 @@ resource "aws_ecs_cluster_capacity_providers" "this" {
   default_capacity_provider_strategy {
     capacity_provider = "FARGATE_SPOT"
     weight            = 2
+  }
+}
+
+# Task Definition
+
+locals {
+
+  container_definitions = [
+    {
+      name      = "app"
+      image     = var.uri_repository
+      cpu       = 0
+      essential = true
+
+      portMappings = [
+        {
+          name          = "app-${var.container_port}-tcp"
+          containerPort = var.container_port
+          hostPort      = var.container_port
+          protocol      = "tcp"
+          appProtocol   = "http"
+        }
+      ]
+    }
+  ]
+}
+
+resource "aws_ecs_task_definition" "service" {
+  family                = "service"
+  container_definitions = jsonencode(local.container_definitions)
+
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+
+  cpu    = "1024"
+  memory = "2048"
+
+  # Execution role como en tu JSON
+  execution_role_arn = var.execution_role_arn
+  # task_role_arn    = var.task_role_arn 
+
+  ephemeral_storage {
+    size_in_gib = 21
+  }
+
+  # runtimePlatform (Linux + x86_64)
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
   }
 }
