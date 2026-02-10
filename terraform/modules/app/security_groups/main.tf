6f2d03528f4e26709af2a626bcfc11a686942181
Scarus <41557299+Eduard20CR@users.noreply.github.com>
2026-02-10 11:49:15 -0600

    Update app security group description and adjust ingress rule to allow traffic on port 8000

--- terraform/modules/app/security_groups/main.tf
+++ terraform/modules/app/security_groups/main.tf
@@ -34,7 +34,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_alb" {
 
 resource "aws_security_group" "app" {
   name        = "${var.project_identifier}-app-sg"
-  description = "Allow TLS inbound traffic and all outbound traffic"
+  description = "Allow HTTP inbound traffic and all outbound traffic"
   vpc_id      = var.vpc_id
 
   tags = {
@@ -47,17 +47,8 @@ resource "aws_vpc_security_group_ingress_rule" "app_from_alb_http" {
   referenced_security_group_id = aws_security_group.alb.id
 
   ip_protocol = "tcp"
-  from_port   = 80
-  to_port     = 80
-}
-
-resource "aws_vpc_security_group_ingress_rule" "app_from_alb_https" {
-  security_group_id            = aws_security_group.app.id
-  referenced_security_group_id = aws_security_group.alb.id
-
-  ip_protocol = "tcp"
-  from_port   = 443
-  to_port     = 443
+  from_port   = 8000
+  to_port     = 8000
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_app" {
